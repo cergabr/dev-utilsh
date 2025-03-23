e9b88bd19558495361f3b0fe8426a83fca393009
Gabriele Cerqueglini <gabriele.cerqueglini@codebaker.it>
2025-03-23 16:20:21 +0100

    fix: fix source package info in init.sh, update version cmd

--- lib/init.sh
+++ lib/init.sh
@@ -2,5 +2,6 @@
 # Load all library components or expose functions
 
 # Source the utilities
+source "${BASH_SOURCE%/*}/../package.sh"
 source "${BASH_SOURCE%/*}/colors.sh"
 source "${BASH_SOURCE%/*}/logging.sh"
--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 export BINS="bin/utilsh"
 export LIBS="lib"
 export PKG_NAME="dev-utilsh"
-export PKG_VERSION="0.0.2"
+export PKG_VERSION="0.0.3"
 export PKG_DESCRIPTION="Dev utility for shell scripts with colorized logging"
 export PKG_AUTHOR="Gabriele Cerqueglini"
 export PKG_EMAIL="gabriele@cerqueglini.com"
